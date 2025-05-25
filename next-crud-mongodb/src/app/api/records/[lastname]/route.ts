82962482967788eaa2af63a99c7a5df6f4f4a1a9
mlasitsa <mlasitsa@css.edu>
2025-05-25 14:00:16 -0500

    test 4

--- next-crud-mongodb/src/app/api/records/[lastname]/route.ts
+++ next-crud-mongodb/src/app/api/records/[lastname]/route.ts
@@ -1,8 +1,10 @@
-import { NextRequest } from "next/server";
-import { NextResponse } from "next/server";
-import clientPromise from "@/lib/db";
+import { NextRequest, NextResponse } from 'next/server';
+import clientPromise from '@/lib/db';
 
-export async function GET(req: NextRequest, context: { params: { lastname: string } }) {
+export async function GET(
+  req: NextRequest,
+  context: { params: { lastname: string } }
+) {
   try {
     const client = await clientPromise;
     const db = client.db('records');
@@ -13,14 +15,12 @@ export async function GET(req: NextRequest, context: { params: { lastname: strin
       .toArray();
 
     if (!finalData) {
-      return NextResponse.json({ message: "Not Found" });
+      return NextResponse.json({ message: 'Not Found' }, { status: 404 });
     }
 
-    return NextResponse.json({
-      dbPosts: finalData,
-    });
+    return NextResponse.json({ dbPosts: finalData });
   } catch (err) {
-    console.log(err);
-    return NextResponse.json({ message: "ERROR" }, { status: 500 });
+    console.error(err);
+    return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
   }
 }
